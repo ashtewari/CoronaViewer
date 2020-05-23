df13e56018975c354571df67e4e68ef1059729ae
Ash Tewari <ash.tewari.name@gmail.com>
2020-05-23 10:21:08 -0400

    Add chart

--- CoronaViewer/MainPage.xaml.cs
+++ CoronaViewer/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
+using Syncfusion.UI.Xaml.Charts;
+using System.Collections.Generic;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -16,9 +18,61 @@ namespace UwpOnRp
         {
             this.InitializeComponent();
 
-            // https://covid19api.com
-            
-            DataContext = this;
+            SfChart chart = new SfChart() { Header = "Chart", Height = 300, Width = 500 };
+
+            //Adding horizontal axis to the chart 
+            CategoryAxis primaryAxis = new CategoryAxis();
+            primaryAxis.Header = "Name";
+            primaryAxis.FontSize = 14;
+            chart.PrimaryAxis = primaryAxis;
+
+            //Adding vertical axis to the chart 
+            NumericalAxis secondaryAxis = new NumericalAxis();
+            secondaryAxis.Header = "Height(in cm)";
+            secondaryAxis.FontSize = 14;
+            chart.SecondaryAxis = secondaryAxis;
+
+            //Adding Legends for the chart
+            ChartLegend legend = new ChartLegend();
+            chart.Legend = legend;
+
+            //Initializing column series
+            ColumnSeries series = new ColumnSeries();
+            series.ItemsSource = (new ViewModel()).Data;
+            series.XBindingPath = "Name";
+            series.YBindingPath = "Height";
+            series.ShowTooltip = true;
+            series.Label = "Heights";
+
+            //Setting adornment to the chart series
+            series.AdornmentsInfo = new ChartAdornmentInfo() { ShowLabel = true };
+
+            //Adding Series to the Chart Series Collection
+            chart.Series.Add(series);
+            this.Content = chart;
         }
+    }
+
+    public class ViewModel
+    {
+        public List<Person> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new List<Person>()
+            {
+                new Person { Name = "David", Height = 180 },
+                new Person { Name = "Michael", Height = 170 },
+                new Person { Name = "Steve", Height = 160 },
+                new Person { Name = "Joel", Height = 182 }
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public double Height { get; set; }
     }
 }
